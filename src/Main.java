669ca8690b6dc9c5682f3922dddc9a8cbf91795a
Islam <islam.bedretdinov@yandex.ru>
2022-11-23 22:32:03 +0300

    ДЗ №13-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author = new Author("А.С.Пушкин");
+        Book book = new Book("Капитанская дочка",author, 1836);
+        System.out.println("book = " + book.getBookName());
+        System.out.println("author = " + author.getNameAuthor());
+        System.out.println("year = " + book.getPublishingYear());
+        book.setPublishingYear(1835);
+        System.out.println("year = " + book.getPublishingYear());
+
+        Author author1 = new Author("Ф.М. Достоевский");
+        Book book1 = new Book("Преступление и наказание",author1, 1866);
+        System.out.println("book1 = " + book1.getBookName());
+        System.out.println("author1 = " + author1.getNameAuthor());
+        System.out.println("year1 = " + book1.getPublishingYear());
+        book.setPublishingYear(1865);
+        System.out.println("year1 = " + book.getPublishingYear());
     }
 }
